ab9ba3b354ef6bf6e8c07212b56a530cf61b2046
Josue-Gonzalez-Juan <josgonzalez@csumb.edu>
2021-02-19 18:04:57 -0800

    List of Users Shown
    The admin activity now shows a list of all the users in the local machine's database(including the admin's account)

--- app/src/main/java/com/group1/project1/AdminActivity.java
+++ app/src/main/java/com/group1/project1/AdminActivity.java
@@ -1,13 +1,36 @@
 package com.group1.project1;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.group1.project1.data.UserAdapter;
+import com.group1.project1.data.User;
+
+import java.util.List;
 
 public class AdminActivity extends AppCompatActivity {
+
+    RecyclerView userRecycler;
+    UserAdapter userAdapter;
+    AppDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        userRecycler = findViewById(R.id.user_recycler);
+
+        db = AppDatabase.create(this);
+
+        List<User> users = db.getUserDao().getUsers();
+
+        userAdapter = new UserAdapter(users);
+        userRecycler.setAdapter(userAdapter);
+        userRecycler.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/group1/project1/data/UserAdapter.java
@@ -0,0 +1,60 @@
+package com.group1.project1.data;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.group1.project1.AdminActivity;
+import com.group1.project1.R;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    private List<User> usernames;
+
+    public UserAdapter(List<User> list){
+        this.usernames = list;
+    }
+
+    @Override
+    public UserAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View view = inflater.inflate(R.layout.inventory_item, viewGroup, false);
+        UserAdapter.ViewHolder holder = new UserAdapter.ViewHolder(view);
+        return holder;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String s = usernames.get(position).getUsername();
+        holder.getTextView().setText(s);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return usernames.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+        public Button deleteButton;
+
+        public ViewHolder(View view){
+            super(view);
+            textView = view.findViewById(R.id.item_name);
+            deleteButton = view.findViewById(R.id.item_select_button);
+        }
+
+        public TextView getTextView(){
+            return textView;
+        }
+    }
+}
